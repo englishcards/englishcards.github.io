688c5d6dc149fd462a5a56c6d529ea6863a079c1
englishcards <65217826+englishcards@users.noreply.github.com>
2020-05-13 15:24:19 +0300

    added input-writting js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const wordRu = document.querySelectorAll('.word__title_ru');
 const wordEng = document.querySelectorAll('.word__title_eng');
 const wordVoice = document.querySelectorAll('.word__voice');
-
 const wordWrap = document.querySelectorAll('.word-wrap');
+
+const writingInput = document.querySelectorAll('.writing__input');
+const writingBtn = document.querySelectorAll('.writing__btn');
+const writingWrap = document.querySelectorAll('.writing-wrap');
+
 const wordSection = document.querySelectorAll('.word-section');
 
 
@@ -10,9 +14,13 @@ function openWord(arr, elem) {
 	for(let number in arr) {
 		wordRu[elem].style.display = 'block';
 		arr[elem].classList.toggle('hidden');
+
+		if (wordWrap[elem].style.display = 'none') {
+			wordVoice[elem].style.display = 'none';
+			wordEng[elem].style.display = 'none';
+		}
 	}
 }
-
 
 function changeSection() {
 	for(let i = 0; wordSection.length > i; i++) {
@@ -27,6 +35,7 @@ function changeSection() {
 
 		document.querySelector('.word__btn_right').onclick = () =>  {
 			let last = wordSection.length - 1;
+			let previous = count;
 
 			wordSection[count].nextElementSibling.style.display = 'block';
 			wordSection[count].style.display = 'none';
@@ -57,8 +66,37 @@ function changeSection() {
 					wordSection[count].style.display = 'block';
 				}	
 			}
-		}	
+		}
+
+		function checkInput() {
+			for(let number in writingBtn) {
+				writingBtn[number].onclick = () => {
+					let input = writingInput[count].value.toLowerCase();
+					let answer = wordEng[count].innerText.toLowerCase();
+
+					if(input == answer) {
+						writingBtn[count].classList.add('checked');
+						writingInput[count].style.color = '#36b536';
+						writingInput[count].readOnly = true;
+						wordRu[count].innerText = answer;
+						wordRu[count].style.textTransform = 'capitalize';
+					}
+					else if (input == "") {
+						writingInput[count].style.borderColor = '#ff4a4a';
+					}
+					else {
+						writingBtn[count].classList.add('unchecked');
+						writingInput[count].style.color = '#ff4a4a';
+						writingInput[count].readOnly = true;
+						wordRu[count].innerText = answer;
+						wordRu[count].style.textTransform = 'capitalize';
+					}
+				}
+			}
+		}		
 	}
+
+checkInput();	
 }
 		
 
@@ -72,6 +110,5 @@ function makeRandomInt(min, max) {
 }
 
 
-				
 
 changeSection();
