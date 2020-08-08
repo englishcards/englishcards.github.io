422065f111473b1b9ab712af4d1cc84a559a1404
englishcards <65217826+englishcards@users.noreply.github.com>
2020-08-08 13:15:17 +0300

    fixed a modal

--- script.js
+++ script.js
@@ -5,15 +5,40 @@ function openPassword() {
 		let overlay = document.createElement('div');
 		document.body.appendChild(overlay);
 		overlay.classList.add('test-modal');
-		overlay.innerHTML = '<div class="modal-window"><p class="test-modal__text">Please put a password</p><span class="password-input-wrap"><input class="password-input" type="password"><span class="password-error" hidden>Invalid password</span></span><a class="word__btn password__btn password__btn_confirm">Confirm</a><a class="word__btn password__btn password__btn_cancel">Cancel</a></div>';
+		overlay.innerHTML = '<div class="modal-window"><p class="test-modal__text">Please put a password</p><span class="password-input-wrap"><input class="password-input" type="password" autofocus><span class="password-error" hidden>Invalid password</span></span><div class="btn-wrap"><a class="word__btn password__btn password__btn_confirm">Confirm</a><a class="word__btn password__btn password__btn_cancel">Cancel</a></div></div>';
 
 		let confirmBtn = document.querySelector('.password__btn_confirm');
 		let passwordInput = document.querySelector('.password-input');
+		let passwordError = document.querySelector('.password-error');
+		passwordInput.focus();
+
+		window.addEventListener('keyup', function (e) {
+		  	if (e.keyCode === 13) {
+		    	e.preventDefault();
+		   		if (passwordInput.value == 'i') {
+					confirmBtn.setAttribute('href', 'individual/index.html');
+					document.location.href = "https://englishcards.github.io/individual/index.html";
+				}
+				else if (passwordInput.value == '') {
+					passwordError.innerText = 'Can\'t be blank' ;
+					passwordError.hidden = false;
+					passwordInput.style.borderColor = '#ff0000';
+					return false;
+				}
+				else {
+					passwordError.innerText = 'Invalid password';
+					passwordError.hidden = false;
+					passwordInput.style.borderColor = '#ff0000';
+					return false;
+				}
+		  	}
+		  	else if (e.keyCode === 27) {
+		  		overlay.remove();
+		  	}
+		});
 
 		passwordInput.onblur = () => {
-			let passwordError = document.querySelector('.password-error');
 			if (passwordInput.value == 'i') {
-				console.log(passwordInput.value);
 				confirmBtn.onclick = () => confirmBtn.setAttribute('href', 'individual/index.html');
 			}
 			else if (passwordInput.value == '') {
