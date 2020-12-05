061a7a9ecf28a41463424e843506e8874696ebc3
englishcards <65217826+englishcards@users.noreply.github.com>
2020-12-05 10:10:14 +0200

    added a draw

--- katya/js_unit/words.js
+++ katya/js_unit/words.js
@@ -7,6 +7,15 @@ const wordsAll = [
 },
 {
 	unit: 'Unit-2',
+	wordsEng: ['2Camera', '2Mobile phone', '2Shampoo', '2Diary', '2Passport'],
+	wordsRu: ['2Фотоаппарат', '2Мобильный телефон', '2Шампунь', '2Дневник', '2Паспорт'],
+	wordsSound: [`ˈkæmərə`, `ˈməʊbaɪl fəʊn`, `ʃæmˈpuː`, `ˈdaɪərɪ`, `ˈpæspɔːrt`],
+},
+{
+	unit: 'Unit-3',
+	wordsEng: ['3Camera', '3Mobile phone', '3Shampoo', '3Diary'],
+	wordsRu: ['3Фотоаппарат', '3Мобильный телефон', '3Шампунь', '3Дневник'],
+	wordsSound: [`ˈkæmərə`, `ˈməʊbaɪl fəʊn`, `ʃæmˈpuː`, `ˈdaɪərɪ`],
 },
 
 ];
