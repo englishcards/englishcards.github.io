198d7ba84128f3784c91920631cdcfd9517dd779
englishcards <65217826+englishcards@users.noreply.github.com>
2020-12-12 09:01:07 +0200

    add random for verbs

--- katya/verbs/verbs.js
+++ katya/verbs/verbs.js
@@ -3,15 +3,17 @@ const verbsInput = document.querySelectorAll('.verbs__input');
 const verbsSign = document.querySelectorAll('.verbs-answer__sign');
 const verbsCorrect = document.querySelectorAll('.verbs-answer__correct');
 const verbsBtn = document.querySelectorAll('.verbs__btn');
+const verbsCount = document.querySelector('.verbs__count');
+const verbsRandom = document.querySelector('.verbs__random');
 
-let randomValue;
+let count = -1;
 showNewVerbs();
 
 verbsBtn[0].onclick = checkTotal;
 function checkTotal() {
-	checkVerbs(0,verbs.firstForm[randomValue]);
-	checkVerbs(1,verbs.secondForm[randomValue]);
-	checkVerbs(2,verbs.thirdForm[randomValue]);
+	checkVerbs(0,verbs.firstForm[count]);
+	checkVerbs(1,verbs.secondForm[count]);
+	checkVerbs(2,verbs.thirdForm[count]);
 }
 function checkVerbs(index, form) {
 	verbsSign[index].hidden = false;
@@ -26,11 +28,16 @@ function checkVerbs(index, form) {
 
 verbsBtn[1].onclick = showNewVerbs;
 function showNewVerbs() {
-	randomValue = getRandom();
+	if (count == verbs.verbsRu.length - 1) count = -1;
+
+	if (verbsRandom.classList.contains('random-unchecked')) count++;
+	else count = getRandom();
+	
+	verbsCount.innerText = `${count+1} / ${verbs.verbsRu.length}`;
 	defaultAnswer(0);
 	defaultAnswer(1);
 	defaultAnswer(2);
-	verbsExample.innerText = verbs.verbsRu[randomValue];
+	verbsExample.innerText = verbs.verbsRu[count];
 }
 
 function defaultAnswer(index) {
@@ -38,6 +45,23 @@ function defaultAnswer(index) {
 	verbsCorrect[index].style.display = 'none';
 	verbsInput[index].value = '';
 	verbsInput[index].disabled = false;
+}
+
+verbsRandom.onclick = checkRandom;
+function checkRandom() {
+	if (verbsRandom.classList.contains('random-unchecked')) {
+		verbsRandom.classList.remove('random-unchecked');
+		verbsRandom.style.color = '#b105ff';
+		count = getRandom();
+		showNewVerbs();
+	}
+	else {
+		verbsRandom.classList.add('random-unchecked');
+		verbsRandom.style.color = '#3f3f3f';
+		count = -1;
+		showNewVerbs();
+	}
+	
 }
 
 function getRandom() {
