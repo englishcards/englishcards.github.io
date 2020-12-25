45eeaf72c6b75642af13a3bab4f105227cfd3f87
englishcards <65217826+englishcards@users.noreply.github.com>
2020-12-25 11:00:23 +0200

    add 3 unit

--- katya/js_unit/lesson-data.js
+++ katya/js_unit/lesson-data.js
@@ -237,6 +237,92 @@ const unitAll = [
 					</ul>	
 				</div>`,
 },
+{
+	unit: 'Unit-3',
+	gramar: `<h2 class="main-title">Past Simple (was, were)</h2>
+				<ul>
+					<li class="gramar-item gramar-item__img">Subject <span>+</span> was/were <span>+</span> object<br>
+						<p class="example-item example-item_main">He was a student<span class="example-item_triangle"></span></p>
+						<ul class="example-list">
+							<li class="example-item">I was a student</li>
+							<li class="example-item">You were a student</li>
+						</ul>
+					</li>
+					<li class="gramar-item gramar-item__img gramar-item_minus">Subject <span>+</span> was/were <span>+</span> not <span>+</span> object<br>
+						<p class="example-item example-item_main">I wasn't in Rome<span class="example-item_triangle"></span></p>
+						<ul class="example-list">
+							<li class="example-item">They were not there</li>
+							<li class="example-item">She wasn't at work</li>
+						</ul>
+					</li>
+					<li class="gramar-item gramar-item__img gramar-item_mark">Was/were <span>+</span> Subject <span>+</span> object?<br>
+						<p class="example-item example-item_main">Were you in a bank yesterday?<span class="example-item_triangle"></span></p>
+						<ul class="example-list">
+							<li class="example-item">Was he in the theater last week?</li>
+							<li class="example-item">Were they at home in the morning?</li>
+						</ul>
+					</li>
+				</ul>
+				<h2 class="main-title">Past Simple (verbs)</h2>
+				<ul>
+					<li class="gramar-item gramar-item__img">Subject <span>+</span> verb (-ed, II-form) <span>+</span> object<br>
+						<p class="example-item example-item_main">We went to the cinema<span class="example-item_triangle"></span></p>
+						<ul class="example-list">
+							<li class="example-item">She worked yesterday</li>
+							<li class="example-item">I studied at university</li>
+						</ul>
+					</li>
+					<li class="gramar-item gramar-item__img gramar-item_minus">Subject <span>+</span> did <span>+</span> not <span>+</span> verb (I-form) <span>+</span> object<br>
+						<p class="example-item example-item_main">I didn't see her yesterday<span class="example-item_triangle"></span></p>
+						<ul class="example-list">
+							<li class="example-item">We didn't like football</li>
+							<li class="example-item">They didn't call us</li>
+						</ul>
+					</li>
+					<li class="gramar-item gramar-item__img gramar-item_mark">Did <span>+</span> Subject <span>+</span> verb (I-form) <span>+</span> object?<br>
+						<p class="example-item example-item_main">Did you watch TV yesterday?<span class="example-item_triangle"></span></p>
+						<ul class="example-list">
+							<li class="example-item">Did she make her homework?</li>
+							<li class="example-item">Did they went on holiday last year?</li>
+						</ul>
+					</li>
+				</ul>`,
+	words: `Another file`,			
+	text: `<h2 class="main-title text-title">My week</h2>
+				<p class="text-paragraph">Hello, everyone! Jack is speaking.<img src="units/3/text/0.jpg" style="float: right; width:180px; padding-left: 10px;"></p>
+				<p class="text-paragraph">Today I'm going to tell you about my last week.</p>
+				<p class="text-paragraph">On Monday I was in New York. I ate a big hamburger, drank coca-cola and watched The Statue of Liberty. I was there three days. I love New York.</p>
+				<p class="text-paragraph"><img src="units/3/text/1.jpg" style="float: left; width:210px; padding-right: 10px;">On Thursday I came to Miami. I thought it was the hottest time in my life. The temperature was 40 degrees above zero. But you know what, I liked Miami too. I took a book, sat near the ocean and read. My dream came true.</p>
+				<p class="text-paragraph">On Friday I back home. By the way, I met Celine Dion at the airport of Miami. Yeah, that journey was unforgettable</p>
+				<p class="text-paragraph">What about your weekend? Write your comment and subscribe to my channel. Bye!</p>`,
+	practice: ``,
+	listening: `<div class="listening-wrap">
+					<div class="track-list">
+						<h2 class="main-title track-list_header">Track-1</h2>
+					</div>
+					<div class="dialogue">
+						<img src="units/3/audio/0.jpg" width="400px;">
+					</div>
+					<div class="audio-player-wrap">
+						<audio class="audio-player"></audio>
+						<progress class="listening__progress" value="0" max="100"></progress>
+						<span class="listening__current-time"></span>
+						<span class="listening__duration">0:00</span>
+						<input class="listening__volume" type="range"  min="0" max="100">
+						<span class="listening__text">Hide Text</span>
+						<button class="listening__play">Play</button>
+					</div>
+				</div>`,
+	homework: `<div class="homework-wrap">
+					<h2 class="main-title">Homework</h2>
+					<ul class="homework-list">
+						<li class="gramar-item">Learn new words from Unit 3</li>
+						<li class="gramar-item">Work Book: Exercise 1 and 2A, 2B page 37</li>
+						<li class="gramar-item">Work Book: Exercise 1B page 38</li>
+						<li class="gramar-item">Learn irregular verbs</li>
+					</ul>	
+				</div>`,
+},
 
 ];
 
